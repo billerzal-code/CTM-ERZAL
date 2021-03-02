e7281b4d31bcf36064581ae2418d21da48860f43
Bill Erzal <bill@erzalenterprises.com>
2021-03-01 18:17:14 -0600

    initial commit

---
+++ erzalcmt/Controllers/cmtController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace erzalcmt.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class cmtController : ControllerBase
+    {
+
+        private readonly ILogger<cmtController> _logger;
+
+        public cmtController(ILogger<cmtController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public ActionResult GetStringArray(int Number)
+        {
+            List<string> returnStrings = new List<string>();
+            try
+            {
+                for (int count = 1; count <= Number; count++)
+                {
+                    //checking for both multiples of 3 and 5
+                    if (count % 3 == 0 && count % 5 == 0)
+                    {
+                        returnStrings.Add("FizzBuzz");
+                    }
+                    //checking for multiple of 3
+                    else if (count % 3 == 0)
+                    {
+                        returnStrings.Add("Fizz");
+                    }
+                    //checking for multiple of 5
+                    else if (count % 5 == 0)
+                    {
+                        returnStrings.Add("Buzz");
+                    }
+                    //just add string representation of count...
+                    else
+                    {
+                        returnStrings.Add(count.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //log the exception..
+                _logger.LogError(ex.ToString());
+                //return the error...
+                return BadRequest(ex.ToString());
+            }
+
+            return Ok(returnStrings.ToArray());
+
+        }
+    }
+}
---
+++ erzalcmt/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
+
+namespace erzalcmt
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "Place Info Service API",
+                    Version = "v2",
+                    Description = "Sample service for Learner",
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwagger();
+            app.UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v2/swagger.json", "PlaceInfo Services"));
+            app.UseHttpsRedirection();
+            
+        }
+    }
+}
